9b4e872f4f9df7bcf5cd8499a6c294e05ed2c9c9
Chris Sanders <sanders.chris@gmail.com>
2017-03-25 15:00:31 -0500

    minion installs

---
+++ reactive/salt_minion.py
@@ -0,0 +1,10 @@
+from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import status_set
+from charmhelpers.fetch import apt_install
+
+@when_not('salt-minion.installed')
+@when('salt-common.installed')
+def install_layer_salt_minion_subordinate():
+    status_set('maintenance','installing salt-minion')
+    apt_install('salt-minion')
+    set_state('salt-minion.installed')
