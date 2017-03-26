a42f5e1bb629381bcede8b42ca24f3dd4e9ca879
Chris Sanders <sanders.chris@gmail.com>
2017-03-26 01:33:57 -0500

    States are ready to apply

--- reactive/salt_minion.py
+++ reactive/salt_minion.py
@@ -1,5 +1,6 @@
 from charms.reactive import when, when_all, when_not, set_state, remove_state
 from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.services.base import service_restart
 from charmhelpers.fetch import apt_install
 
 @when_not('salt-minion.installed')
@@ -11,10 +12,11 @@ def install_layer_salt_minion_subordinate():
     status_set('active','')
 
 @when('salt-minion.installed')
-@when_all('saltmaster.changed')
+@when('saltmaster.changed')
 def configure_master(saltmaster):
     with open('/etc/salt/minion.d/master.conf','w') as conf:
-        conf.write("master: {}".format(saltmaster.hostname))
-    saltmaster.configure_me()
+        conf.write("master: {}".format(saltmaster.address))
+    service_restart('salt-minion')
+    saltmaster.minion_ready()
     remove_state('saltmaster.changed')
     
