2f9f0e32250905cdf44187af01b4d06aa9acd547
Chris Sanders <sanders.chris@gmail.com>
2018-03-18 18:53:13 -0500

    Add unit-id config option

--- reactive/salt_minion.py
+++ reactive/salt_minion.py
@@ -1,4 +1,5 @@
 from charms.reactive import when, when_not, set_state, remove_state
+from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.services.base import service_restart
 from charmhelpers.fetch import apt_install
@@ -7,8 +8,12 @@ from charmhelpers.fetch import apt_install
 @when_not('salt-minion.installed')
 @when('salt-common.installed')
 def install_layer_salt_minion_subordinate():
+    config = hookenv.config()
     status_set('maintenance', 'installing salt-minion')
     apt_install('salt-minion')
+    if config['unit-id']:
+        with open('/etc/salt/minion_id', 'w') as conf:
+            conf.write("{}".format(hookenv.local_unit()))
     set_state('salt-minion.installed')
     status_set('active', 'Minion ready')
 
