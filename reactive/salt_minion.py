854c63529ed8c78ee6acf93a96ad3efce83cd8bc
Chris Sanders <sanders.chris@gmail.com>
2018-03-18 18:08:56 -0500

    Set ready message after insall

--- reactive/salt_minion.py
+++ reactive/salt_minion.py
@@ -1,22 +1,24 @@
-from charms.reactive import when, when_all, when_not, set_state, remove_state
+from charms.reactive import when, when_not, set_state, remove_state
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.services.base import service_restart
 from charmhelpers.fetch import apt_install
 
+
 @when_not('salt-minion.installed')
 @when('salt-common.installed')
 def install_layer_salt_minion_subordinate():
-    status_set('maintenance','installing salt-minion')
+    status_set('maintenance', 'installing salt-minion')
     apt_install('salt-minion')
     set_state('salt-minion.installed')
-    status_set('active','')
+    status_set('active', 'Minion ready')
+
 
 @when('salt-minion.installed')
 @when('saltmaster.changed')
 def configure_master(saltmaster):
-    with open('/etc/salt/minion.d/master.conf','w') as conf:
+    with open('/etc/salt/minion.d/master.conf', 'w') as conf:
         conf.write("master: {}".format(saltmaster.address))
     service_restart('salt-minion')
     saltmaster.minion_ready()
     remove_state('saltmaster.changed')
-    
+
