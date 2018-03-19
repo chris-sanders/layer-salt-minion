67be7da63c47ff220a670c5dddc31f6f619aa2e8
Chris Sanders <sanders.chris@gmail.com>
2018-03-18 21:24:23 -0500

    Register minion id with host via relation

--- reactive/salt_minion.py
+++ reactive/salt_minion.py
@@ -3,18 +3,24 @@ from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.services.base import service_restart
 from charmhelpers.fetch import apt_install
+import socket
+
+
+def get_minion_id():
+    config = hookenv.config()
+    if config['unit-as-id']:
+        return hookenv.local_unit().replace('/', '-')
+    else:
+        return socket.getfqdn()
 
 
 @when_not('salt-minion.installed')
 @when('salt-common.installed')
 def install_layer_salt_minion_subordinate():
-    config = hookenv.config()
     status_set('maintenance', 'installing salt-minion')
     apt_install('salt-minion')
-    if config['unit-as-id']:
-        with open('/etc/salt/minion_id', 'w') as conf:
-            minionId = hookenv.local_unit().replace('/', '-')
-            conf.write("{}".format(minionId))
+    with open('/etc/salt/minion_id', 'w') as conf:
+        conf.write("{}".format(get_minion_id()))
     set_state('salt-minion.installed')
     status_set('active', 'Minion ready')
 
@@ -25,6 +31,6 @@ def configure_master(saltmaster):
     with open('/etc/salt/minion.d/master.conf', 'w') as conf:
         conf.write("master: {}".format(saltmaster.address))
     service_restart('salt-minion')
-    saltmaster.minion_ready()
+    saltmaster.minion_ready(get_minion_id())
     remove_state('saltmaster.changed')
 
