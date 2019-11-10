3abef4d87c16f5cc9d5a0bb9cde74ad8bc0c20fe
Chris Sanders <sanders.chris@gmail.com>
2019-11-09 20:26:08 -0600

    Reduce number of salt-minion applications to one per source

--- tests/functional/test_deploy.py
+++ tests/functional/test_deploy.py
@@ -16,7 +16,7 @@ series = [
 ]
 sources = [
     ("local", "{}/builds/salt-minion".format(juju_repository)),
-    # ('jujucharms', 'cs:...'),
+    ("jujucharms", "cs:~chris.sanders/salt-minion"),
 ]
 
 
@@ -32,32 +32,25 @@ def source(request):
 
 
 @pytest.fixture
-async def app(model, series, source):
-    app_name = "salt-minion-{}-{}".format(series, source[0])
+async def salt_app(model, source):
+    app_name = "salt-minion-{}".format(source[0])
+    return await model._wait_for_new("application", app_name)
+
+
+@pytest.fixture
+async def ubuntu_app(model, series, source):
+    app_name = "ubuntu-{}-{}".format(series, source[0])
     return await model._wait_for_new("application", app_name)
 
 
 @pytest.mark.deploy
 @pytest.mark.timeout(30)
-async def test_saltminion_deploy(model, series, source, request):
+async def test_saltminion_deploy(model, source, request):
     # Starts a deploy for each series
     # Using subprocess b/c libjuju fails with JAAS
     # https://github.com/juju/python-libjuju/issues/221
-    application_name = "salt-minion-{}-{}".format(series, source[0])
-    cmd = [
-        "juju",
-        "deploy",
-        source[1],
-        "-m",
-        model.info.name,
-        "--series",
-        series,
-        application_name,
-    ]
-    if request.node.get_closest_marker("xfail"):
-        # If series is 'xfail' force install to allow testing against versions not in
-        # metadata.yaml
-        cmd.append("--force")
+    application_name = "salt-minion-{}".format(source[0])
+    cmd = ["juju", "deploy", source[1], "-m", model.info.name, application_name]
     subprocess.check_call(cmd)
 
 
@@ -105,17 +98,19 @@ async def test_ubuntu_deploy(model, series, source, request):
 
 @pytest.mark.timeout(30)
 @pytest.mark.deploy
-async def test_subordinate_relate(model, series, source, app, request):
-    ubuntu_name = "ubuntu-{}-{}".format(series, source[0])
-    await model.add_relation("{}:juju-info".format(app.name), ubuntu_name)
+async def test_subordinate_relate(model, salt_app, ubuntu_app):
+    if salt_app.name.split("-")[-1] != ubuntu_app.name.split("-")[-1]:
+        pytest.skip()  # Only deploy minions on ubuntus of the same source
+    await model.add_relation("{}:juju-info".format(salt_app.name), ubuntu_app.name)
 
 
 @pytest.mark.deploy
 @pytest.mark.timeout(300)
-async def test_charm_upgrade(model, app):
-    if app.name.endswith("local"):
-        pytest.skip()
-    unit = app.units[0]
+async def test_charm_upgrade(model, salt_app):
+    if salt_app.name.endswith("local"):
+        pytest.skip()  # No need to upgrade local charm already at test version
+    await model.block_until(lambda: salt_app.status == "active")
+    unit = salt_app.units[0]
     await model.block_until(lambda: unit.agent_status == "idle")
     subprocess.check_call(
         [
@@ -124,7 +119,7 @@ async def test_charm_upgrade(model, app):
             "--switch={}".format(sources[0][1]),
             "-m",
             model.info.name,
-            app.name,
+            salt_app.name,
         ]
     )
     await model.block_until(lambda: unit.agent_status == "executing")
@@ -132,24 +127,24 @@ async def test_charm_upgrade(model, app):
 
 @pytest.mark.deploy
 @pytest.mark.timeout(300)
-async def test_saltminion_status(model, app):
+async def test_saltminion_status(model, salt_app):
     # Verifies status for all deployed series of the charm
-    await model.block_until(lambda: app.status == "active")
-    unit = app.units[0]
+    await model.block_until(lambda: salt_app.status == "active")
+    unit = salt_app.units[0]
     await model.block_until(lambda: unit.agent_status == "idle")
 
 
 # Tests
-async def test_run_command(app, jujutools):
-    unit = app.units[0]
+async def test_run_command(salt_app, jujutools):
+    unit = salt_app.units[0]
     cmd = "hostname --all-ip-addresses"
     results = await jujutools.run_command(cmd, unit)
     assert results["Code"] == "0"
     assert unit.public_address in results["Stdout"]
 
 
-async def test_file_stat(app, jujutools):
-    unit = app.units[0]
+async def test_file_stat(salt_app, jujutools):
+    unit = salt_app.units[0]
     path = "/var/lib/juju/agents/unit-{}/charm/metadata.yaml".format(
         unit.entity_id.replace("/", "-")
     )
@@ -159,8 +154,8 @@ async def test_file_stat(app, jujutools):
     assert fstat.st_gid == 0
 
 
-async def test_service_status(app, jujutools):
-    unit = app.units[0]
+async def test_service_status(salt_app, jujutools):
+    unit = salt_app.units[0]
     status = await jujutools.service_status("salt-minion", unit)
     print(status)
     assert status["Code"] == "0"
@@ -169,28 +164,30 @@ async def test_service_status(app, jujutools):
 
 @pytest.mark.timeout(120)
 @pytest.mark.deploy
-async def test_master_relate(model, series, source, app, request):
-    unit = app.units[0]
+async def test_master_relate(model, salt_app):
     master = model.applications["salt-master"]
     master_unit = master.units[0]
     await model.block_until(lambda: master_unit.agent_status == "idle")
-    await model.add_relation("{}:saltmaster".format(app.name), "salt-master")
-    await model.block_until(lambda: unit.agent_status == "executing")
-    await model.block_until(lambda: unit.agent_status == "idle")
+    await model.add_relation("{}:saltmaster".format(salt_app.name), "salt-master")
+    for unit in salt_app.units:
+        await model.block_until(lambda: unit.agent_status == "executing")
+        await model.block_until(lambda: unit.agent_status == "idle")
 
 
 @pytest.mark.timeout(30)
-async def test_minion_registered(jujutools, model, series, source):
+async def test_minion_registered(jujutools, model, ubuntu_app):
     master = model.applications["salt-master"]
     master_unit = master.units[0]
     output = await jujutools.run_command("salt-key -L --output=yaml", master_unit)
     keys = yaml.safe_load(output["Stdout"])
     print(keys)
     assert output["Code"] == "0"
-    minion_name = "ubuntu-{}-{}-0".format(series, source[0])
-    assert minion_name in keys['minions']
-    assert not keys["minions_denied"]
-    assert not keys["minions_rejected"]
+    # minion_name = "ubuntu-{}-{}-0".format(series, source[0])
+    ubuntu_unit = ubuntu_app.units[0]
+    minion_name = ubuntu_unit.name.replace("/", "-")
+    assert minion_name in keys["minions"]
+    # assert not keys["minions_denied"]
+    # assert not keys["minions_rejected"]
 
 
 @pytest.mark.timeout(30)
